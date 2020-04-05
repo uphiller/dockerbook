4ef44b69c06ab5610d33495b22bffeb784476749
Ubuntu <kube@kube-master.114lveiwrwlerldrpkyiyrhgfh.syx.internal.cloudapp.net>
2020-04-05 04:14:17 +0000

    add ubuntu setup script

--- kubernetes-setup.sh
+++ kubernetes-setup.sh
@@ -9,8 +9,13 @@ software-properties-common
 
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
+#sudo add-apt-repository \
+#"deb [arch=amd64] https://download.docker.com/linux/debian \
+#$(lsb_release -cs) \
+#stable"
+
 sudo add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/debian \
+"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
 $(lsb_release -cs) \
 stable"
 
