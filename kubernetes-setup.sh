b187b9a8f882df2016d2e2045d626efb60d8d8d0
Ubuntu <kube@kube-master.ftofu0wat33urm50moxxq3a2ta.syx.internal.cloudapp.net>
2020-05-03 00:22:13 +0000

    modify

--- kubernetes-setup.sh
+++ kubernetes-setup.sh
@@ -7,12 +7,7 @@ curl \
 gnupg2 \
 software-properties-common
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
-#sudo add-apt-repository \
-#"deb [arch=amd64] https://download.docker.com/linux/debian \
-#$(lsb_release -cs) \
-#stable"
+curl -fsSL https://download.docker.com/linux/ununtu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
