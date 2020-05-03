a5576f773f39eb7f2e05a80b829e1b94f89f5fb1
YoungHo Kwak <jason.uphiller@gmail.com>
2020-05-03 09:31:48 +0900

    Update kubernetes-setup.sh
    modify

--- kubernetes-setup.sh
+++ kubernetes-setup.sh
@@ -7,7 +7,7 @@ curl \
 gnupg2 \
 software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ununtu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository \
 "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
