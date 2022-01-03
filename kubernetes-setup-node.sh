b4f379386a40f3c3338b499b3c26e1f980b9a1e0
YoungHo Kwak <jason.uphiller@gmail.com>
2022-01-03 16:27:29 +0900

    Update kubernetes-setup-node.sh

--- kubernetes-setup-node.sh
+++ kubernetes-setup-node.sh
@@ -1,11 +1,6 @@
 sudo apt-get update
 
-sudo apt-get -y install \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg2 \
-software-properties-common
+sudo apt-get install -y ca-certificates curl gnupg lsb-release software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -20,15 +15,21 @@ sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 sudo usermod -aG docker $USER
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+sudo mkdir /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
 EOF
 
-sudo apt-get update
+sudo systemctl enable docker
 
-sudo apt-get install -y kubelet kubeadm kubectl
+sudo systemctl daemon-reload
 
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl restart docker
 
