8e1811675f59bbf29f86e15d8806344da4f7fe41
YoungHo Kwak <jason.uphiller@gmail.com>
2022-07-07 13:42:14 +0900

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -17,3 +17,5 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo usermod -aG docker $USER
