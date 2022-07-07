e34fab844c21bcc7a53a0fb53d77d300c7330450
YoungHo Kwak <jason.uphiller@gmail.com>
2022-07-07 16:34:19 +0900

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -22,4 +22,4 @@ sudo usermod -aG docker $USER
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-
+sudo chmod +x /usr/local/bin/docker-compose
