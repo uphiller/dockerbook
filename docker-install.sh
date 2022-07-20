7958713d2e3a0ceab0fcb64ad63b9c3df8814c57
YoungHo Kwak <jason.uphiller@gmail.com>
2022-07-20 10:08:50 +0900

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -16,7 +16,7 @@ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo usermod -aG docker $USER
 
