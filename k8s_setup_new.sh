6a5721bd0a0794aa9436080e7f5ff6c18286661d
YoungHo Kwak <jason.uphiller@gmail.com>
2023-05-10 11:39:45 +0900

    Update k8s_setup_new.sh

--- k8s_setup_new.sh
+++ k8s_setup_new.sh
@@ -101,7 +101,7 @@ sudo apt-get install -y containerd.io
 
 sudo vi /etc/containerd/config.toml
 #disabled_plugins = ["cri"] 수정
-systemctl restart containerd
+sudo systemctl restart containerd
 
 /etc/sysctl.conf
 net.ipv4.ip_forward=1 수정
