cc96e4384d84dc2096d15d45602a52bb2784933b
YoungHo Kwak <jason.uphiller@gmail.com>
2023-05-10 13:59:43 +0900

    Update k8s_setup_new.sh

--- k8s_setup_new.sh
+++ k8s_setup_new.sh
@@ -45,8 +45,12 @@ kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/
 watch kubectl get pods --all-namespaces
 
 # Argocd
+kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 kubectl get svc -n argocd
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+#Jekins
+kubectl create namespace jenkins
 
