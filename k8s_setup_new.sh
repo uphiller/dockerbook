ef2c42c5e6f710d50eb697d2f575145212305d75
YoungHo Kwak <jason.uphiller@gmail.com>
2023-05-10 11:32:38 +0900

    Update k8s_setup_new.sh

--- k8s_setup_new.sh
+++ k8s_setup_new.sh
@@ -122,6 +122,14 @@ sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 
 
+kdir -p $HOME/.kube
+
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
 명령어 
 
 watch kubectl get pods --all-namespaces
