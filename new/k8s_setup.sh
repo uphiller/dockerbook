a0b4d843efb39c4118ead6f718cd68bcb8fa7c5c
YoungHo Kwak <jason.uphiller@gmail.com>
2023-05-12 15:06:44 +0900

    Update k8s_setup.sh

--- new/k8s_setup.sh
+++ new/k8s_setup.sh
@@ -17,7 +17,12 @@ sudo systemctl restart containerd
 sudo vi /etc/sysctl.conf
 net.ipv4.ip_forward=1 수정
 sudo sysctl -p
+
 sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
